c0a714235a50ce8d817e3cb14508b76d944bc6b9
Sean Tan <stjr03@gmail.com>
2025-06-07 23:07:57 +0800

    fixed import error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@
 
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import ActivityPage from "./ActivityPage";
+import ActivityPage from "./pages/ActivityPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
